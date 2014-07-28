383ef929f06ed816d41d0c7b7a8090cf00d62980
Monica Colangelo <monica.colangelo@gmail.com>
2014-07-28 14:12:28 +0000

    added java-gateway conf file

--- manifests/java_gateway.pp
+++ manifests/java_gateway.pp
@@ -4,4 +4,12 @@ class zabbix_server::java_gateway {
     ensure => present
   }
   
+  file { '/etc/zabbix/zabbix_java_gateway.conf':
+    content => template('zabbix_server/zabbix_java_gateway.conf.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644'
+  }
+  
+  
 }
